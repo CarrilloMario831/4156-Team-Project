00e15345ecb5412f1fb8a575823658be3e69deaf
Sebastian Jimenez <123037060+sjimenez814@users.noreply.github.com>
2024-10-15 22:42:52 -0400

    Add Spotless linter to Maven lifecycle, add Github workflows (#13)
    * Added lombok dependency and cleaned up file structure
    
    * Fix file structure to get rid of empty root folder
    
    * Add Spotless linter to Maven lifecycle, add Github workflows

--- src/main/java/service/service/ServiceApplication.java
+++ src/main/java/service/service/ServiceApplication.java
@@ -1,13 +1,13 @@
-package Service.Service;
+package service.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Sample JavaDoc to pass checkstyle. */
 @SpringBootApplication
 public class ServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceApplication.class, args);
+  }
 }

dfc7f01d3020b42c7ccebc16dc0d820d6fbd862d
CarrilloMario831 <carrillomario831@gmail.com>
2024-10-16 22:32:34 -0400

    javadoc for routecontroller.

--- Service/src/main/java/Service/Service/RouteController.java
+++ Service/src/main/java/Service/Service/RouteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+/* This class contains all the API routes for the service. */
 @RestController
 public class RouteController {
 

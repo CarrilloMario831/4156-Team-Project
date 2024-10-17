e613dc020e25edc4cd44a14a97655f8346799e56
CarrilloMario831 <carrillomario831@gmail.com>
2024-10-16 23:25:52 -0400

    changed inventory to lombok.

--- src/main/java/service/models/Inventory.java
+++ src/main/java/service/models/Inventory.java
@@ -3,31 +3,25 @@ package service.models;
 import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
 /**
  * Represents an inventory that holds a collection of items.
  */
+@Getter
+@Builder
 public class Inventory {
 
-  private final UUID uuid;
+  @NonNull private final UUID inventoryId = UUID.randomUUID();
 
-  private String inventoryName;
-  private Map<UUID, Item> items;
-  private final String adminId;
+  @NonNull @Setter private String inventoryName;
 
-  /**
-   * Constructs a new Inventory with the specified administrator ID, inventory name, and items.
-   *
-   * @param adminId       the UUID of the administrator
-   * @param inventoryName the name of the inventory
-   * @param items         a map of items in the inventory
-   */
-  public Inventory(UUID adminId, String inventoryName, Map<UUID, Item> items) {
-    this.uuid = UUID.randomUUID();
-    this.inventoryName = inventoryName;
-    this.items = items;
-    this.adminId = adminId.toString();
-  }
+  @NonNull private Map<UUID, Item> items;
+
+  @NonNull private final UUID adminId;
 
   /**
    * Creates a new item and adds it to the inventory.
@@ -42,17 +36,9 @@ public class Inventory {
             .itemName(itemName)
             .quantity(quantity)
             .location(location)
-            .price(price).build();
+            .price(price)
+            .build();
     addItem(item);
-  }
-
-  /**
-   * Returns the UUID of the inventory.
-   *
-   * @return the UUID of the inventory
-   */
-  public UUID getId() {
-    return uuid;
   }
 
   /**
@@ -93,41 +79,14 @@ public class Inventory {
   }
 
   /**
-   * Returns the name of the inventory.
-   *
-   * @return the inventory name
-   */
-  public String getInventoryName() {
-    return inventoryName;
-  }
-
-  /**
-   * Sets the name of the inventory.
-   *
-   * @param inventoryName the new inventory name
-   */
-  public void setInventoryName(String inventoryName) {
-    this.inventoryName = inventoryName;
-  }
-
-  /**
-   * Returns the administrator ID associated with the inventory.
-   *
-   * @return the administrator ID
-   */
-  public String getAdminId() {
-    return adminId;
-  }
-
-  /**
    * Returns a string representation of the inventory.
    *
-   * @return a string representing of the inventory
+   * @return a string representation of the inventory
    */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append(inventoryName).append(" (").append(uuid).append("):\n");
+    sb.append(inventoryName).append(" (").append(inventoryId).append("):\n");
     for (Map.Entry<UUID, Item> entry : items.entrySet()) {
       Item value = entry.getValue();
       sb.append("Item Name: ")
